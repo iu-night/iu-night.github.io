8eb574d5adafa371ffedce8f7f8b95bedc2d2efc
iu-night <iu.git@outlook.com>
2022-07-11 21:24:04 +0800

    docs: 📝home components idea

---
+++ docs/assets/test.f07eab8f.js
@@ -0,0 +1,1 @@
+import{_ as o,o as n,c,b as t}from"./app.ddfec27f.js";const r={},a={class:"flex flex-col mt-10px justify-center"},l=t("div",{class:"font-bold text-30px"}," test page ",-1);function d(s,e){return n(),c("div",a,[l,t("div",null,[t("button",{btn:"","text-sm":"",m:"3 t8",onClick:e[0]||(e[0]=f=>s.$router.back())}," \u8FD4\u56DE ")])])}var _=o(r,[["render",d]]);export{_ as default};
