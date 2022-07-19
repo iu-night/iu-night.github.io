fa433a37ebeeb193d50b7f3cd652d3edcfc947e7
iu-night <iu.git@outlook.com>
2022-07-19 17:54:53 +0800

    docs: 📝

---
+++ docs/assets/card.07f3feec.js
@@ -0,0 +1,20 @@
+import{_ as m,o as c,l as i,w as e,m as o,z as f,d as C,r as x,c as h,a as t,e as r,b as I,A as D,n as w}from"./app.33906580.js";import{_ as S}from"./CodeBox.ab81742f.js";const v=`<template>\r
+  <IuCard title="Card\u6807\u9898">\r
+    <div>\u5361\u7247\u5185\u5BB9</div>\r
+  </IuCard>\r
+</template>\r
+`,B=`<template>\r
+  <IuCard>\r
+    <template #header>\r
+      #header\r
+    </template>\r
+    default\r
+    <template #footer>\r
+      #footer\r
+    </template>\r
+    <template #action>\r
+      #action\r
+    </template>\r
+  </IuCard>\r
+</template>\r
+`,E={},$=o(" #header "),b=o(" default "),g=o(" #footer "),k=o(" #action ");function A(_,d){const a=f;return c(),i(a,null,{header:e(()=>[$]),footer:e(()=>[g]),action:e(()=>[k]),default:e(()=>[b]),_:1})}const N=m(E,[["render",A]]),V={class:"flex flex-col mt-30px"},F=I("div",{"mb-30px":"","text-30px":"","font-bold":""}," Card ",-1),z=o(" \u5728\u5199\u4E86\u5728\u5199\u4E86... "),q=C({__name:"card",setup(_){return(d,a)=>{const s=D,p=x("Starport"),n=S,u=N,l=w;return c(),h("div",V,[F,t(l,null,{default:e(()=>[t(n,{"w-500px":"",title:"\u57FA\u7840",code:r(v)},{default:e(()=>[t(p,{port:"iu-card","w-300px":"","h-100px":""},{default:e(()=>[t(s)]),_:1})]),_:1},8,["code"]),t(n,{"w-500px":"",title:"\u60AC\u6D6E",code:""},{default:e(()=>[z]),_:1}),t(n,{"w-500px":"",title:"\u63D2\u69FD",code:r(B)},{default:e(()=>[t(u)]),_:1},8,["code"])]),_:1})])}}});export{q as default};
