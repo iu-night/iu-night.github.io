c3cfa486a7d79da12cc7040dc89bb20a81a64569
iu-night <iu.git@outlook.com>
2022-07-22 17:38:55 +0800

    docs: 📝

---
+++ docs/assets/button.c23f59a8.js
@@ -0,0 +1,33 @@
+import{i as h,o as a,c as p,k as f,_ as I,l as b,w as e,a as t,m as r,I as x,n as l,d as y,r as S,e as _,b as w,p as g}from"./app.a0b798aa.js";import{_ as C}from"./CodeBox.f150cacd.js";const $={class:"prose prose-sm text-left"},v=f("<h2>API</h2><h3>Button Props</h3><table><thead><tr><th>\u540D\u79F0</th><th>\u7C7B\u578B</th><th>\u9ED8\u8BA4\u503C</th><th>\u8BF4\u660E</th></tr></thead><tbody><tr><td>type</td><td><code>primary | success | warning | error</code></td><td><code>primary</code></td><td>\u6309\u94AE\u7684\u7C7B\u578B</td></tr><tr><td>disabled</td><td><code>boolean</code></td><td><code>false</code></td><td>\u6309\u94AE\u662F\u5426\u7981\u7528</td></tr></tbody></table>",3),k=[v],D={__name:"ButtonProps",setup(d,{expose:s}){return s({frontmatter:{meta:[]}}),h({meta:[]}),(u,c)=>(a(),p("div",$,k))}},N={},P=r(" \u7981\u7528 "),V=r(" \u7981\u7528 "),A=r(" \u7981\u7528 "),E=r(" \u7981\u7528 ");function F(d,s){const n=x,o=l;return a(),b(o,null,{default:e(()=>[t(n,{disabled:""},{default:e(()=>[P]),_:1}),t(n,{disabled:"",type:"warning"},{default:e(()=>[V]),_:1}),t(n,{disabled:"",type:"error"},{default:e(()=>[A]),_:1}),t(n,{disabled:"",type:"success"},{default:e(()=>[E]),_:1})]),_:1})}const H=I(N,[["render",F]]),T=`<template>\r
+  <IuSpace>\r
+    <IuButton type="primary">\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+    <IuButton type="warning">\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+    <IuButton type="error">\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+    <IuButton type="success">\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+  </IuSpace>\r
+</template>\r
+`,j=`<template>\r
+  <IuSpace>\r
+    <IuButton disabled>\r
+      \u7981\u7528\r
+    </IuButton>\r
+    <IuButton disabled type="warning">\r
+      \u7981\u7528\r
+    </IuButton>\r
+    <IuButton disabled type="error">\r
+      \u7981\u7528\r
+    </IuButton>\r
+    <IuButton disabled type="success">\r
+      \u7981\u7528\r
+    </IuButton>\r
+  </IuSpace>\r
+</template>\r
+`,q={class:"flex flex-col mt-30px"},z=w("div",{"mb-30px":"","text-30px":"","font-bold":""}," Button ",-1),K=y({__name:"button",setup(d){return(s,n)=>{const o=g,u=S("Starport"),c=C,i=H,m=l,B=D;return a(),p("div",q,[z,t(m,{vertical:""},{default:e(()=>[t(c,{"w-500px":"",title:"\u57FA\u7840",code:_(T)},{default:e(()=>[t(u,{port:"iu-button","w-296px":"","h-34px":""},{default:e(()=>[t(o)]),_:1})]),_:1},8,["code"]),t(c,{"w-500px":"",title:"\u7981\u7528",code:_(j)},{default:e(()=>[t(i)]),_:1},8,["code"])]),_:1}),t(B)])}}});export{K as default};
---
+++ docs/assets/card.8ccccd67.js
@@ -0,0 +1,25 @@
+import{_ as l,o as _,l as p,w as e,m as n,z as u,b as i,d as h,r as v,c as I,a as t,e as c,A as b,n as w}from"./app.a0b798aa.js";import{_ as S}from"./CodeBox.f150cacd.js";const $=`<template>\r
+  <IuCard title="Card\u6807\u9898">\r
+    <div>\u5361\u7247\u5185\u5BB9</div>\r
+  </IuCard>\r
+</template>\r
+`,B=`<template>\r
+  <IuCard>\r
+    <template #header>\r
+      #header\r
+    </template>\r
+    default\r
+    <template #footer>\r
+      #footer\r
+    </template>\r
+    <template #action>\r
+      #action\r
+    </template>\r
+  </IuCard>\r
+</template>\r
+`,D=`<template>\r
+  <IuCard hoverable>\r
+    <div>\u53EF\u60AC\u6D6E\u5361\u7247</div>\r
+  </IuCard>\r
+</template>\r
+`,g={},k=n(" #header "),A=n(" default "),N=n(" #footer "),V=n(" #action ");function E(d,s){const o=u;return _(),p(o,null,{header:e(()=>[k]),footer:e(()=>[N]),action:e(()=>[V]),default:e(()=>[A]),_:1})}const F=l(g,[["render",E]]),H={},z=i("div",null,"\u53EF\u60AC\u6D6E\u5361\u7247",-1);function T(d,s){const o=u;return _(),p(o,{hoverable:""},{default:e(()=>[z]),_:1})}const j=l(H,[["render",T]]),q={class:"flex flex-col mt-30px"},y=i("div",{"mb-30px":"","text-30px":"","font-bold":""}," Card ",-1),K=h({__name:"card",setup(d){return(s,o)=>{const m=b,f=v("Starport"),r=S,C=j,a=w,x=F;return _(),I("div",q,[y,t(a,null,{default:e(()=>[t(a,{vertical:""},{default:e(()=>[t(r,{"w-500px":"",title:"\u57FA\u7840",code:c($)},{default:e(()=>[t(f,{port:"iu-card","w-300px":"","h-100px":""},{default:e(()=>[t(m)]),_:1})]),_:1},8,["code"]),t(r,{"w-500px":"",title:"\u60AC\u6D6E",code:c(D)},{default:e(()=>[t(C)]),_:1},8,["code"])]),_:1}),t(a,{vertical:""},{default:e(()=>[t(r,{"w-500px":"",title:"\u63D2\u69FD",code:c(B)},{default:e(()=>[t(x)]),_:1},8,["code"])]),_:1})]),_:1})])}}});export{K as default};
