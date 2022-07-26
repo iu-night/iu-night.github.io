9a708b432820304982e2be09398c1696e47c84ab
iu-night <iu-night@outlook.com>
2022-07-26 20:25:17 +0800

    docs: 📝

---
+++ docs/assets/tooltip.ae527926.js
@@ -0,0 +1,17 @@
+import{d as c,r as a,o as _,c as s,a as t,w as o,e as l,b as i,E as r,n as m}from"./app.67601856.js";import{_ as d}from"./CodeBox.328ba3f9.js";const f=`<template>
+  <IuSpace>
+    <IuButton v-tooltip.left="'\u663E\u793A\u5728 \u5DE6'">
+      \u5DE6
+    </IuButton>
+    <IuButton v-tooltip.top="'\u663E\u793A\u5728 \u4E0A'">
+      \u4E0A
+    </IuButton>
+    <IuButton v-tooltip.bottom="'\u663E\u793A\u5728 \u4E0B'">
+      \u4E0B
+    </IuButton>
+    <IuButton v-tooltip.right="'\u663E\u793A\u5728 \u53F3'">
+      \u53F3
+    </IuButton>
+  </IuSpace>
+</template>
+`,x={class:"flex flex-col mt-30px"},B=i("div",{"mb-30px":"","text-30px":"","font-bold":""}," Button ",-1),b=c({__name:"tooltip",setup(I){return(v,h)=>{const n=r,e=a("Starport"),p=d,u=m;return _(),s("div",x,[B,t(u,{vertical:""},{default:o(()=>[t(p,{"w-500px":"",title:"\u57FA\u7840",code:l(f)},{default:o(()=>[t(e,{port:"iu-tooltip","w-236px":"","h-34px":""},{default:o(()=>[t(n)]),_:1})]),_:1},8,["code"])]),_:1})])}}});export{b as default};
