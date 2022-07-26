672ab7a946081b062fe92ad807dc607d735d6c49
iu-night <iu.git@outlook.com>
2022-07-26 17:58:21 +0800

    docs: 📝

---
+++ docs/assets/button.8fb047c7.js
@@ -0,0 +1,36 @@
+import{i as h,o as u,c as p,k as f,_ as I,l as b,w as e,a as t,m as r,I as x,n as l,d as y,r as S,e as _,b as w,p as g}from"./app.340e5a4d.js";import{_ as C}from"./CodeBox.a150f724.js";const $={class:"prose prose-sm text-left"},v=f("<h2>API</h2><h3>Button Props</h3><table><thead><tr><th>\u540D\u79F0</th><th>\u7C7B\u578B</th><th>\u9ED8\u8BA4\u503C</th><th>\u8BF4\u660E</th></tr></thead><tbody><tr><td>type</td><td><code>default | primary | success | warning | error</code></td><td><code>default</code></td><td>\u6309\u94AE\u7684\u7C7B\u578B</td></tr><tr><td>disabled</td><td><code>boolean</code></td><td><code>false</code></td><td>\u6309\u94AE\u662F\u5426\u7981\u7528</td></tr></tbody></table>",3),k=[v],A={__name:"ButtonApi",setup(a,{expose:s}){return s({frontmatter:{meta:[]}}),h({meta:[]}),(d,c)=>(u(),p("div",$,k))}},D={},N=r(" \u7981\u7528 "),V=r(" \u7981\u7528 "),P=r(" \u7981\u7528 "),E=r(" \u7981\u7528 ");function F(a,s){const n=x,o=l;return u(),b(o,null,{default:e(()=>[t(n,{disabled:""},{default:e(()=>[N]),_:1}),t(n,{disabled:"",type:"warning"},{default:e(()=>[V]),_:1}),t(n,{disabled:"",type:"error"},{default:e(()=>[P]),_:1}),t(n,{disabled:"",type:"success"},{default:e(()=>[E]),_:1})]),_:1})}const H=I(D,[["render",F]]),T=`<template>\r
+  <IuSpace>\r
+    <IuButton>\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+    <IuButton type="primary">\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+    <IuButton type="warning">\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+    <IuButton type="error">\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+    <IuButton type="success">\r
+      \u5F00\u59CB\r
+    </IuButton>\r
+  </IuSpace>\r
+</template>\r
+`,j=`<template>\r
+  <IuSpace>\r
+    <IuButton disabled>\r
+      \u7981\u7528\r
+    </IuButton>\r
+    <IuButton disabled type="warning">\r
+      \u7981\u7528\r
+    </IuButton>\r
+    <IuButton disabled type="error">\r
+      \u7981\u7528\r
+    </IuButton>\r
+    <IuButton disabled type="success">\r
+      \u7981\u7528\r
+    </IuButton>\r
+  </IuSpace>\r
+</template>\r
+`,q={class:"flex flex-col mt-30px"},z=w("div",{"mb-30px":"","text-30px":"","font-bold":""}," Button ",-1),K=y({__name:"button",setup(a){return(s,n)=>{const o=g,d=S("Starport"),c=C,i=H,m=l,B=A;return u(),p("div",q,[z,t(m,{vertical:""},{default:e(()=>[t(c,{"w-500px":"",title:"\u57FA\u7840",code:_(T)},{default:e(()=>[t(d,{port:"iu-button","w-296px":"","h-34px":""},{default:e(()=>[t(o)]),_:1})]),_:1},8,["code"]),t(c,{"w-500px":"",title:"\u7981\u7528",code:_(j)},{default:e(()=>[t(i)]),_:1},8,["code"])]),_:1}),t(B)])}}});export{K as default};
---
+++ docs/assets/tooltip.31fb064e.js
@@ -0,0 +1,17 @@
+import{_ as m,t as f,o as e,l as c,w as t,x as _,m as u,I as x,n as a,d as v,r as B,c as I,a as r,e as h,b as S}from"./app.340e5a4d.js";import{_ as b}from"./CodeBox.a150f724.js";const w=`<template>\r
+  <IuSpace>\r
+    <IuButton v-tooltip.left="'\u663E\u793A\u5728 \u5DE6'">\r
+      \u5DE6\r
+    </IuButton>\r
+    <IuButton v-tooltip.top="'\u663E\u793A\u5728 \u4E0A'">\r
+      \u4E0A\r
+    </IuButton>\r
+    <IuButton v-tooltip.bottom="'\u663E\u793A\u5728 \u4E0B'">\r
+      \u4E0B\r
+    </IuButton>\r
+    <IuButton v-tooltip.right="'\u663E\u793A\u5728 \u53F3'">\r
+      \u53F3\r
+    </IuButton>\r
+  </IuSpace>\r
+</template>\r
+`,C={},g=u(" \u5DE6 "),D=u(" \u4E0A "),k=u(" \u4E0B "),N=u(" \u53F3 ");function T(l,p){const n=x,s=a,o=f("tooltip");return e(),c(s,null,{default:t(()=>[_((e(),c(n,null,{default:t(()=>[g]),_:1})),[[o,"\u663E\u793A\u5728 \u5DE6",void 0,{left:!0}]]),_((e(),c(n,null,{default:t(()=>[D]),_:1})),[[o,"\u663E\u793A\u5728 \u4E0A",void 0,{top:!0}]]),_((e(),c(n,null,{default:t(()=>[k]),_:1})),[[o,"\u663E\u793A\u5728 \u4E0B",void 0,{bottom:!0}]]),_((e(),c(n,null,{default:t(()=>[N]),_:1})),[[o,"\u663E\u793A\u5728 \u53F3",void 0,{right:!0}]])]),_:1})}const V=m(C,[["render",T]]),$={class:"flex flex-col mt-30px"},A=S("div",{"mb-30px":"","text-30px":"","font-bold":""}," Button ",-1),j=v({__name:"tooltip",setup(l){return(p,n)=>{const s=V,o=B("Starport"),i=b,d=a;return e(),I("div",$,[A,r(d,{vertical:""},{default:t(()=>[r(i,{"w-500px":"",title:"\u57FA\u7840",code:h(w)},{default:t(()=>[r(o,{port:"iu-tooltip","w-296px":"","h-34px":""},{default:t(()=>[r(s)]),_:1})]),_:1},8,["code"])]),_:1})])}}});export{j as default};
