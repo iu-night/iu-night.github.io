08e56ea424513f8f748a0fb589d127549f846bd6
iu-night <iu-night@users.noreply.github.com>
2023-01-13 08:59:03 +0000

    deploy: iu-night/IU-UI@f230ee707bfb778c338cef0da8adaa078010155a

---
+++ assets/DocPopover-baf12cd2.js
@@ -0,0 +1,14 @@
+const n=`<script setup lang="ts">
+</script>
+
+<template>
+  <IuSpace>
+    <IuPopover>
+      <template #trigger>
+        <IuButton>hover</IuButton>
+      </template>
+      <span>hover ing</span>
+    </IuPopover>
+  </IuSpace>
+</template>
+`;export{n as default};
