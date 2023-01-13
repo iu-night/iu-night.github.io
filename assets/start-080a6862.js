b47ec1b5ca5c678ea59b52aee439029e46e331a8
iu-night <iu-night@users.noreply.github.com>
2023-01-13 09:27:33 +0000

    deploy: iu-night/IU-UI@bf6a53bdb1154f3794ea46490abfa961d97cf952

---
+++ assets/start-080a6862.js
@@ -0,0 +1,8 @@
+import{_ as a,y as o,o as r,c,q as t,z as e,a as m}from"./app-e33809dc.js";const _={class:"prose prose-sm text-left"},p=m("h2",null,"开始…",-1),k=[],s={__name:"start",setup(d,{expose:n}){return n({frontmatter:{meta:[]}}),o({meta:[]}),(i,u)=>(r(),c("div",_,[p,t(` ## 安装
+
+#### npm i
+
+
+## 如何使用
+
+### 直接引入 `),t(" ```html\n\n```  "),t("  ")]))}};typeof e=="function"&&e(s);const x=a(s,[["__file","/home/runner/work/IU-UI/IU-UI/src/pages/guide/start.md"]]);export{x as default,k as meta};
